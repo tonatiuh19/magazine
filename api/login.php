6e1779ea6633b06e1e688b59e85a5a47456b7e7e
Gomez <tonatiuh.gomez@hp.com>
2021-03-16 13:57:58 -0600

    adding api

---
+++ api/login.php
@@ -0,0 +1,37 @@
+<?php
+require_once('db_cnn/cnn.php');
+$method = $_SERVER['REQUEST_METHOD'];
+
+if($method == 'POST'){
+	$requestBody=file_get_contents('php://input');
+	$params= json_decode($requestBody);
+	$params = (array) $params;
+
+	if ($params['email']) {
+		$email = $params['email'];
+        $pwd = $params['pwd'];
+
+		$sql = "SELECT email, name, last_name, type, active FROM users WHERE email='$email' AND pwd='$pwd' and active=1";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+			//echo 'Hola';
+			//echo $result;
+			while($row = $result->fetch_assoc()) {
+				$array[] = array_map('utf8_encode', $row);
+			}
+			$res = json_encode($array, JSON_NUMERIC_CHECK);
+			header('Content-Type: application/json');
+			echo $res;
+		} else {
+			echo "0";
+		}
+	}else{
+		echo "Not valid Body Data";
+	}
+
+}else{
+	echo "Not valid Data";
+}
+
+$conn->close();
+?>
