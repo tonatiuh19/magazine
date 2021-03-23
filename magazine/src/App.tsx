9d1a1b08724bba4583229202de2f8c35e6548b99
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2021-03-22 20:56:21 -0600

    clean

--- magazine/src/App.tsx
+++ magazine/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Science from './components/Science/Science';
@@ -8,34 +8,58 @@ import Posts from './components/Posts/Posts';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 import Profile from './components/Posts/Profile';
+import {getPostsTypes} from './apiFunctions/apiFunctions';
+import Loading from './resources/Loading/Loading';
 function App() {
   document.title = 'Agustirri';
+  const [loading, setLoading] = useState(true);
+  const [postsTypes, setPostsTypes] = useState([]);
+
+  useEffect(() => {
+    getPostsTypes().then((y) => {
+      setPostsTypes(y);
+  }).finally(() => setLoading(false));
+  }, [])
+
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark" fixed="top">
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        {/* <Form inline>
-          <FormControl type="text" placeholder="Buscar..." className="mr-sm-2" />
-          <Button variant="outline-info">Buscar</Button>
-        </Form> */}
-        
-      </Navbar>
-      <div className="flex-shrink-0 main-container content">
-        <Router>
-          <Switch>
-            <Route exact path='/' component={Main} />
-            <Route path="/creatives" component={Posts} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/ciencia" component={Science} />
-          </Switch>    
-        </Router>
-      </div>
-      <Footer></Footer>
+      {loading ? (<div id="outer" className="container">
+        <div id="inner" className="row">
+          <div className="col-12 text-center">
+            <Loading></Loading>
+          </div>   
+        </div>
+      </div>): 
+      (
+        <>
+          <Navbar bg="dark" variant="dark" fixed="top">
+          <Navbar.Brand href="#home">Logo</Navbar.Brand>
+          <Nav className="mr-auto">
+            {postsTypes.map((x:any, index:number) =>{
+                return (<Nav.Link key={index} href="/creatives">{x.name}</Nav.Link>);
+            })}
+          </Nav>
+          {/* <Form inline>
+            <FormControl type="text" placeholder="Buscar..." className="mr-sm-2" />
+            <Button variant="outline-info">Buscar</Button>
+          </Form> */}
+          
+        </Navbar>
+        <div className="flex-shrink-0 main-container content">
+          <Router>
+            <Switch>
+              <Route exact path='/' component={Main} />
+              <Route path="/creatives" component={Posts} />
+              <Route path="/profile" component={Profile} />
+              <Route path="/ciencia" component={Science} />
+            </Switch>    
+          </Router>
+        </div>
+        <Footer></Footer>
+      </>
+      )
+    }
+      
     </div>
   );
 }
--- magazine/src/components/Posts/Profile.tsx
+++ magazine/src/components/Posts/Profile.tsx
@@ -1,22 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import './styles/posts.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPowerOff, faPlusCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
-import { BsFillImageFill, BsTextIndentLeft, BsTrash, BsPencil } from 'react-icons/bs';
-import { FiDelete } from 'react-icons/fi';
-import { GrYoutube, GrFacebook, GrInstagram, GrTwitter } from 'react-icons/gr';
-import { getPostsbyUser, getPostsTypes, insertPost, insertPostTypes, insertPostTypesWithImage } from '../../apiFunctions/apiFunctions';
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
 import Loading from '../../resources/Loading/Loading';
-import {Form} from 'react-bootstrap';
-import { useHistory, BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 
-import ImagePost from './ImagePost';
-import EditorPost from './EditorPost';
-import YoutubePost from './YoutubePost';
-import InstagramPost from './InstagramPost';
-import TwitterPost from './TwitterPost';
-import FacebookPost from './FacebookPost';
-import Profile from './Profile';
 
 const Posts = () => {
     const [loading, setLoading] = useState(true);
