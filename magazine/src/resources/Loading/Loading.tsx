1cc2f8471b2e10c4b8991a6cdb98c887e5257c2d
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2021-03-28 17:16:52 -0600

    Update Loading.tsx

--- magazine/src/resources/Loading/Loading.tsx
+++ magazine/src/resources/Loading/Loading.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHatWizard } from '@fortawesome/free-solid-svg-icons';
+import { faAtom } from '@fortawesome/free-solid-svg-icons';
 
 const Loading = () => {
     return (
         <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-            <FontAwesomeIcon icon={faHatWizard} pulse />
+            <FontAwesomeIcon icon={faAtom} pulse />
         </div>
     )
 }
